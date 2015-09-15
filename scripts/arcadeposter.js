9b6ed741c0562ab0dc4eb7537dad10d45ced5b7d
James Baicoianu <james_github@baicoianu.com>
2015-09-15 03:41:43 -0700

    Handle poster load race condition

--- scripts/arcadeposter.js
+++ scripts/arcadeposter.js
@@ -3,10 +3,16 @@ elation.require('engine.things.generic', function() {
     this.setPosterTexture = function(texurl) {
       var tex = elation.engine.materials.getTexture(texurl);
       if (tex) {
-        var oldmat = this.parts['poster'].children[0].material;
-        this.parts['poster'].children[0].material = new THREE.MeshPhongMaterial({
-          map: tex,
-        });
+        if (!this.parts['poster']) {
+          elation.events.add(this, 'thing_load', elation.bind(this, this.setPosterTexture, texurl));
+        } else {
+          var oldmat = this.parts['poster'].children[0].material;
+          this.parts['poster'].children[0].material = new THREE.MeshPhongMaterial({
+            map: tex,
+            normalMap: oldmat.normalMap,
+            lightMap: oldmat.lightMap
+          });
+        }
       }
     }
   }, elation.engine.things.generic);
